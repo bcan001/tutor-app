e0cc4daf0e064a4b444cf42594677ae1c9619b28
Ben Caneba <bcaneba@gmail.com>
2015-07-22 09:21:35 +0800

    new course form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,17 +9,29 @@ class CoursesController < ApplicationController
 	end
 	def new
 		# binding.pry
-		# @course = Course.new
+		@course = Course.new
 		# @school = @course.school
 
 	end
 	def create
-
+		@course = Course.new(course_params)
+		@school = @course.school
+		# binding.pry
+		if @course.save
+			redirect_to school_path(params[:school_id])
+		else
+			render 'new'
+		end
 	end
 	def edit
 	end
 	def update
 	end
 	def destroy
+	end
+
+	private
+	def course_params
+		params.require(:course).permit!
 	end
 end
